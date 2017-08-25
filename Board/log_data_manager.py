f339dc7855f9f3efef8cc5be8fe9e1e9d8ea1aeb
gumbee <mugeeb.hassan@gmail.com>
2017-08-25 17:15:29 +0200

    updated README and broke up parts of the log data manager

--- Board/log_data_manager.py
+++ Board/log_data_manager.py
@@ -9,6 +9,11 @@ from Utils.anomaly_detector import AnomalyDetector as AnomalyDetector
 
 
 def get_neural_networks():
+    """
+    Finds all the neural networks in Logs/NeuralNets
+    
+    :return: A list of all the neural networks
+    """
     neural_nets = []
 
     path = os_path.join(ROOT_DIR, 'Logs/NeuralNets')
@@ -32,6 +37,14 @@ def get_neural_networks():
 
 
 def get_net_info(net_id: str):
+    """
+    Gathers relevant information about the specified neural network, such as trainings performed on that network,
+    input data added to the network (only if the input was manually added to the log) and relvant network architecture
+    information.
+    
+    :param net_id:  The id of the network whose information should be gathered
+    :return:        A dictionary containing the relevant information
+    """
     neural_net = {'trainings': [], 'network_info': [], 'input_data': []}
 
     path = os_path.join(ROOT_DIR, 'Logs/NeuralNets')
@@ -39,10 +52,13 @@ def get_net_info(net_id: str):
     # loop through all the log files in the log's neural net directory
     if os_path.exists(path):
         for log_file_name in os_listdir(path):
+            # only consider log files
             if log_file_name.endswith('.log'):
                 file_path = os_path.join(path, log_file_name)
 
+                # open the log file
                 with open(file_path, 'rb') as log_file:
+                    # load the data that is saved in this log file
                     log_data = pickle.load(log_file)
 
                     # only process the network if it is the right one
@@ -54,43 +70,57 @@ def get_net_info(net_id: str):
                             for input_data in log_data['input_data']:
                                 neural_net['input_data'].append(input_data)
 
-                            layers = neural_net['network_info']['layers']
-                            # the amount of units to be displayed on the board when approximating the shape of the NN
-                            layer_sizes = [0]*len(layers)
-                            # used to calculate the amount of units to be displayed
-                            layer_ratios = []
-
-                            max_units = layers[0]['units']
-                            max_units_index = 0
-
-                            # calculate the ratios between the layers and find the layer with the most units
-                            for i in range(1, len(layers)):
-                                layer_ratios.append((layers[i]['units']*1.0)/layers[i-1]['units'])
-
-                                if max_units < layers[i]['units']:
-                                    max_units = layers[i]['units']
-                                    max_units_index = i
-
-                            # max amount of units to be displayed on the board
-                            layer_sizes[max_units_index] = min(max_units, 7)
-
-                            # calculate the number of units for every layer left to the layer with the most units
-                            # based on the previously calculated ratios
-                            for i in range(max_units_index-1, -1, -1):
-                                layer_sizes[i] = max(1, int(round(layer_sizes[i+1]*(1.0/layer_ratios[i]))))
-
-                            # calculate the number of units for every layer right to the layer with the most units
-                            # based on the previously calculated ratios
-                            for i in range(max_units_index+1, len(layer_sizes)):
-                                layer_sizes[i] = max(1, int(round(layer_sizes[i-1]*layer_ratios[i-1])))
-
-                            neural_net['network_info']['layer_sizes'] = layer_sizes
+                            # get an approximation of the layer sizes (relative to each other) so we can
+                            # display an approximation of the network on the board
+                            neural_net['network_info']['layer_sizes'] = get_net_layer_sizes_approximation(neural_net['network_info'])
 
                         # add all the training sessions
                         for training in log_data['training_sessions']:
                             neural_net['trainings'].append({'session_id': training, 'data': log_data['training_sessions'][training]})
 
     return neural_net
+
+
+def get_net_layer_sizes_approximation(network_info: list):
+    """
+    Takes a neural network info list as input and calculates an approximation of the layer sizes so we can display
+    the layer sizes approximation on the board (because the actual network layers usually have too much units to display
+    nicely on a page)
+    
+    :param network_info:    The network info list
+    :return:                The approximation of the layer sizes
+    """
+    layers = network_info['layers']
+    # the amount of units to be displayed on the board when approximating the shape of the NN
+    layer_sizes = [0] * len(layers)
+    # used to calculate the amount of units to be displayed
+    layer_ratios = []
+
+    max_units = layers[0]['units']
+    max_units_index = 0
+
+    # calculate the ratios between the layers and find the layer with the most units
+    for i in range(1, len(layers)):
+        layer_ratios.append((layers[i]['units'] * 1.0) / layers[i - 1]['units'])
+
+        if max_units < layers[i]['units']:
+            max_units = layers[i]['units']
+            max_units_index = i
+
+    # max amount of units to be displayed on the board
+    layer_sizes[max_units_index] = min(max_units, 7)
+
+    # calculate the number of units for every layer left to the layer with the most units
+    # based on the previously calculated ratios
+    for i in range(max_units_index - 1, -1, -1):
+        layer_sizes[i] = max(1, int(round(layer_sizes[i + 1] * (1.0 / layer_ratios[i]))))
+
+    # calculate the number of units for every layer right to the layer with the most units
+    # based on the previously calculated ratios
+    for i in range(max_units_index + 1, len(layer_sizes)):
+        layer_sizes[i] = max(1, int(round(layer_sizes[i - 1] * layer_ratios[i - 1])))
+
+    return layer_sizes
 
 
 def get_net_training_info(net_id: str, session_id: str):
@@ -138,6 +168,13 @@ def get_net_training_info(net_id: str, session_id: str):
 
 
 def get_anomaly_free_data(entries: list, costs: list):
+    """
+    Takes a list of entries and outputs and finds all anomalies and removes them.
+    
+    :param entries: The entries (x-axis)
+    :param costs:   The outputs (y-axis)
+    :return:        Anomaly free version of the input and output
+    """
     entries = np.matrix(entries).T
     costs = np.matrix(costs).T
     # conserve a copy of the original costs so we can display the original costs on the graph
--- README.md
+++ README.md
@@ -12,75 +12,29 @@
 
 ## Quick guide
 
-The following guide steps you through the process of creating a neural network and visualizing the performance and data. The network will be trained to recognize digits (10 classes, 0-9) using the MNIST data set.
+The following guide steps you through the process of creating a neural network and visualizing the performance and data. The network will be trained to recognize digits (10 classes, 0-9) using the MNIST data set. For a more detailed guide (also on how to visualize the data, performance etc.) take a look at the NotSoQuickGuide.
 
-#### Imports
 ```Python
-# import a data manager who handles getting the MNIST data for us
 import Data.data_manager as DataManager
-# import a gradient descent optimizer so we can train the network with gradient descent
+from NeuralNetwork.neural_network import NeuralNetwork
 from Training.gradient_descent import GradientDescentOptimizer
-```
 
-#### Creating the network
-```Python
 # create a neural network with 784 input units and some hidden layers
 network = NeuralNetwork(784, name="Example Network")
 network.add_hidden_layer(100)
 network.add_hidden_layer(100)
 network.add_hidden_layer(100)
 network.add_output_layer(10)
-```
 
-#### Getting the data
-```Python
 # get the data (data not included, download the data yourself and move it to the Data/DataFiles folder)
-# data can be downloaded from: http://deeplearning.net/data/mnist/mnist.pkl.gz
+# data can be downloaded from: http://deeplearning.net/data/mnist/mnist.pkl.gz
 X, y, X_val, y_val, X_test, y_test = DataManager.get_mnist_data()
-```
 
-#### Training the network
-```Python
-# train the network with the gradient descent optimizer
-network.train(X, y, GradientDescentOptimizer())
-```
+# train the network with the gradient descent optimizer and the specified settings
+network.train(X, y, GradientDescentOptimizer)
 
-#### Outputting the performance
-```Python
 print('Accuracy Training Set: ', network.get_mean_correct(X, y))
 print('Accuracy Validation Set: ', network.get_mean_correct(X_val, y_val))
-```
-
-### Adding visualization
-
-To add visual representations of the performance (accuracy) and the data we need to create a log handler which handles all of that for us. First, we need to extend our imports:
-
-#### Extending the imports
-```Python
-from Logging.logger import LogHandler as LogHandler
-```
-
-#### Creating the log handler
-```Python
-# create a log handler so we can visualize our progress
-log_handler = LogHandler()
-```
-
-#### Adding the data set
-```Python
-# add the training set to the log handler so that we can see a visual interpretation of the data set
-log_handler.add_data_set(X, y, subset_size=5000)
-# add the cross validation set to the log handler so that we can see a visual interpretation of the data set
-log_handler.add_data_set(X_val, y_val, subset_size=5000)
-```
-
-#### Monitor the performance
-```Python
-# add a few data sets whose accuracy performance we want to monitor and visualize on a graph
-# make sure to add these lines before training the network so that we actually capture the
-# performance change during the training
-log_handler.add_accuracy_monitor(X, y, name="Training Set", subset_size=500)
-log_handler.add_accuracy_monitor(X_val, y_val, name="Validation Set", subset_size=500)
 ```
 
 ## License
